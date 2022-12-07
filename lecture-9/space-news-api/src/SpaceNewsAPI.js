e8e2cf9367727bdc94d6b4c753c4c907edb30c6b
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-12-07 14:51:40 -0500

    Converted to use useEffect

--- lecture-9/space-news-api/src/SpaceNewsAPI.js
+++ lecture-9/space-news-api/src/SpaceNewsAPI.js
@@ -1,53 +1,47 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './SpaceNewsAPI.css'
 
-class SpaceNewsAPI extends React.Component {
-  state = {
-    flights: [],
-  }
+const SpaceNewsAPI = () => {
+  const [flights, setFlights] = useState([])
 
-  componentDidMount() {
+  useEffect(() => {
     // Make a GET request to the SpaceX Flights API
     axios
       .get('https://api.spacexdata.com/v2/launches')
       .then((res) => {
         // Set the flights state with the response data
-        this.setState({ flights: res.data })
+        setFlights(res.data)
       })
       .catch((err) => {
         // Handle any errors
         console.error(err)
       })
-  }
+  }, [])
 
-  render() {
-    const { flights } = this.state
-
-    // Map through the flights array and render a list of flights
-    return (
-      <ul className='flights-list'>
-        {flights.map((flight) => (
-          <li key={flight.flight_number}>
-            <div className='flight-info'>
-              <img
-                src={flight.links.mission_patch_small}
-                alt={flight.mission_name}
-              />
-              <div className='flight-data'>
-                <h2>{flight.mission_name}</h2>
-                <p>Flight Number: {flight.flight_number}</p>
-                <p>Launched Date: {flight.launch_date_utc}</p>
-                <p>Flight Details: {flight.details}</p>
-                <p>Launch Year: {flight.launch_year}</p>
-                <a href={flight.links.article_link}>Read about the launch</a>
-              </div>
+  // Map through the flights array and render a list of flights
+  return (
+    <ul className='flights-list'>
+      {flights.map((flight) => (
+        <li key={flight.flight_number}>
+          <div className='flight-info'>
+            <img
+              src={flight.links.mission_patch_small}
+              alt={flight.mission_name}
+            />
+            <div className='flight-data'>
+              <h2>{flight.mission_name}</h2>
+              <p>Flight Number: {flight.flight_number}</p>
+              <p>Launched Date: {flight.launch_date_utc}</p>
+              <p>Flight Details: {flight.details}</p>
+              <p>Launch Year: {flight.launch_year}</p>
+              <a href={flight.links.article_link}>Read about the launch</a>
             </div>
-          </li>
-        ))}
-      </ul>
-    )
-  }
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default SpaceNewsAPI
